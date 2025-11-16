a3c4806c5e6c8e5727b01e6e9400df387015a90e
niloynh <niloyhakim09@gmail.com>
2025-11-16 17:40:19 +0530

    Today I learned JavaScript Prototype & Prototype Chain

---
+++ javascript/01-advanced-concepts/Prototypes/README.md
@@ -0,0 +1,10 @@
+# JavaScript Prototype — Daily Practice
+
+Today’s challenge: Understanding **Prototype** in JavaScript.
+
+### 🚀 What I practiced
+- What is `prototype`
+- What is `__proto__`
+- How objects inherit methods through prototypes
+- Created a simple constructor function with prototype methods
+
---
+++ javascript/01-advanced-concepts/Prototypes/prototypes.js
@@ -0,0 +1,41 @@
+// Basic Prototype Use
+function Person(name){
+    this.name = name
+}
+
+Person.prototype.sayHi = function() {
+    // console.log("Hi, I am" + this.name);
+}
+const p1 = new Person("Niloy")
+const p2 = new Person("Hakim")
+
+p1.sayHi();
+p2.sayHi()
+
+// Array prototype
+Array.prototype.first = function () {
+    return this[2]
+}
+
+// console.log([100, 200, 300].first());
+
+
+// Prototype Chain
+
+function Animal(name) {
+    this.name = name
+}
+
+Animal.prototype.eat = function () {
+    console.log(this.name + " is Eating");
+}
+
+function Dog(name) {
+    Animal.call(this, name)
+}
+
+Dog.prototype = Object.create(Animal.prototype);
+Dog.prototype.constructor = Dog;
+
+const d = new Dog("Rocky")
+d.eat()
