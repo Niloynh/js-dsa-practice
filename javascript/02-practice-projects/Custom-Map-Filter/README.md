9b91984d0e364d940a3ec044d9f486b6bf8b79fa
niloynh <niloyhakim09@gmail.com>
2025-11-18 18:30:31 +0530

    Rebuilt JS map() & filter() from scratch today

---
+++ javascript/02-practice-projects/Custom-Map-Filter/README.md
@@ -0,0 +1,15 @@
+# JS Custom Map & Filter
+
+Rebuilding JavaScript fundamentals from scratch.
+
+## ✔️ Implemented
+- customMap() → works like Array.map()
+- customFilter() → works like Array.filter()
+
+## 🧠 Why?
+To understand how JS higher-order functions work internally and build stronger logic.
+
+## 🚀 Example
+customMap([1,2,3], n => n*2) → [2,4,6]
+
+
---
+++ javascript/02-practice-projects/Custom-Map-Filter/custom-map-filter.js
@@ -0,0 +1,50 @@
+// Built-in map()
+let nums = [1, 2, 3]
+let doubled = nums.map(n => n * 2)
+// console.log(doubled);
+
+// custom map 
+function customMap(arr, callBack){
+    let result = [];
+    for(let i = 0; i < arr.length; i++){
+        result.push(callBack(arr[i], i, arr))
+    }
+    return result;
+}
+
+// Using custom map
+
+let nums2 = [1, 2, 3];
+
+let doubled2 = customMap(nums2, function(n) {
+    return n * 2;
+});
+
+
+// Built-in filter()
+
+let nums3 = [1, 2, 3, 4];
+let even = nums3.filter(n => n % 2 === 0);
+// console.log(even);
+
+
+// custom filter()
+
+function customFilter(arr1, callBack1){
+    let result = [];
+    for(let i = 0; i < arr1.length; i++) {
+    if(callBack1(arr1[i], i, arr1)){
+        result.push(arr1[i])
+    }
+}
+return result;
+}
+
+
+// using 
+let nums4 = [1, 2, 3, 4];
+
+let evenFilter = customFilter(nums4, function(n) {
+    return n % 2 === 0;
+});
+console.log(evenFilter);
